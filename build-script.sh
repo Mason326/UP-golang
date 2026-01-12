0553c8a60f0027ae162f0e135158b1160507c800
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2026-01-12 21:23:03 +0300

    Enhanced auto-build script

--- build-script.sh
+++ build-script.sh
@@ -1,20 +1,29 @@
+#!/bin/bash
+# Аргумент с названием директории
 directory="$1"
 if [ -n "$directory" ] && [ -d "$directory" ]
 then
-	targetScripts=`ls "$directory"`
-	echo ""
-	for file in $targetScripts
+	cd "$directory"
+	targetScripts=`ls | grep -E "*go"`
+	# Создаем go.mod если его нет
+	if [ ! -f "go.mod" ]; then
+ 	   	go mod init myapp
+	fi
+
+	# Собираем каждый .go файл отдельно
+	for gofile in $targetScripts
 	do
-		go build "$file"
-		echo "Builded $file"
+		# Отделяем расширение от имени файла
+        	exename="${gofile%.go}"
+        	echo "Building $exename"
+        	go build -o "$exename" "$gofile"
 	done
-	
+
 	echo ""
 	echo "Build successful"
-	echo ""	
+	echo ""
 	exit 0
 else
 	echo "You must to provide a directory name"
 	exit 1
 fi
-
